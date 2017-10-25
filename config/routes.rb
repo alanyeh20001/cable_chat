be90e28bfadbb6fb0154e004360f11779632b65b
alanyeh20001 <alanyeh20001@hotmail.com>
2017-10-26 02:01:08 +0800

    Preparation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'chat_rooms#index'
+  resources :chat_rooms, only: [:new, :create, :show, :index]
 end
