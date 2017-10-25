65a3b2434fc5687faeb082f83d2ab67d11517992
alanyeh20001 <alanyeh20001@hotmail.com>
2017-10-26 01:37:02 +0800

    Migrate, add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025172433) do
+ActiveRecord::Schema.define(version: 20171025173453) do
 
   create_table "chat_rooms", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20171025172433) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
